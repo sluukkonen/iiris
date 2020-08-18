ee93e151207774b9555ecd20aa19b566860bc735
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2020-08-18 22:07:13 +0300

    Polyfill Object.entries for Node 10 for now, add isFunction

--- src/fromEntries.js
+++ src/fromEntries.js
@@ -1,1 +1,14 @@
-export const fromEntries = Object.fromEntries
+import { isFunction } from './isFunction'
+
+export const fromEntries = isFunction(Object.fromEntries)
+  ? /* istanbul ignore next */ Object.fromEntries
+  : /* istanbul ignore next */ function fromEntries(entries) {
+      const result = {}
+
+      for (let i = 0; i < entries.length; i++) {
+        const [key, value] = entries[i]
+        result[key] = value
+      }
+
+      return result
+    }
