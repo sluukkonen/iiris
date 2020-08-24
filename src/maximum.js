9ce045a53477b3b8601d72e5069af4d2074db4fb
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2020-08-24 18:38:44 +0300

    Rewrite maximum/minimum in terms of reduce1 and minU/maxU

--- src/maximum.js
+++ src/maximum.js
@@ -1,4 +1,4 @@
-import { identity } from './identity'
-import { maximumByU } from './internal/maximumByU'
+import { maxU } from './internal/maxU'
+import { reduce1 } from './internal/reduce1'
 
-export const maximum = (array) => maximumByU(identity, array)
+export const maximum = (array) => reduce1(maxU, array)
--- src/minimum.js
+++ src/minimum.js
@@ -1,4 +1,4 @@
-import { minimumByU } from './internal/minimumByU'
-import { identity } from './identity'
+import { reduce1 } from './internal/reduce1'
+import { minU } from './internal/minU'
 
-export const minimum = (array) => minimumByU(identity, array)
+export const minimum = (array) => reduce1(minU, array)
