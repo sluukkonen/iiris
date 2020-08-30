a61dbdc150cacaf7baad4224736ad2a558cf9a97
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2020-08-30 13:56:05 +0300

    Return default value from getOr if the value is undefined

--- src/internal/getOrObject.js
+++ src/internal/getOrObject.js
@@ -1,13 +1,10 @@
 import { isUndefined } from '../isUndefined'
 import { isObject } from '../isObject'
-import { hasOwn } from './hasOwn'
 
 export const getOrObject = (defaultValue, key, object) => {
   if (isObject(object)) {
     const maybeValue = object[key]
-    return isUndefined(maybeValue) && !hasOwn(object, key)
-      ? defaultValue
-      : maybeValue
+    return isUndefined(maybeValue) ? defaultValue : maybeValue
   }
 
   return defaultValue
