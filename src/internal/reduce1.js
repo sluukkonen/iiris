980d0728c2fab18ad0d4782ad97befcd09e3b062
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2021-02-05 18:20:02 +0200

    Do not go out-of-bounds in reduce1

--- src/internal/reduce1.js
+++ src/internal/reduce1.js
@@ -2,7 +2,7 @@
  * Note that this version of reduce1 returns undefined for an empty array.
  */
 export const reduce1 = (fn, array) => {
-  let acc = array[0]
+  let acc = array.length > 0 ? array[0] : undefined
 
   for (let i = 1; i < array.length; i++) {
     acc = fn(acc, array[i])
