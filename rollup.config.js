83bc7af57ae26552c410faa564251f6311c13085
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2021-03-11 23:32:53 +0200

    Minify the code only during CI lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,33 @@
 import replace from '@rollup/plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 
+const minify = process.env.MINIFY != null
+const plugins = !minify
+  ? []
+  : [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        preventAssignment: true,
+      }),
+      terser({
+        compress: { passes: 2, module: true, unsafe: true },
+      }),
+    ]
+const output = !minify
+  ? [
+      {
+        file: './dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: './dist/index.mjs',
+        format: 'es',
+      },
+    ]
+  : [{ file: './dist/index.min.mjs' }]
+
 export default {
   input: './src/index.js',
-  output: [
-    {
-      file: './dist/index.js',
-      format: 'cjs',
-    },
-    {
-      file: './dist/index.mjs',
-      format: 'es',
-    },
-    {
-      file: './dist/index.min.mjs',
-      format: 'es',
-      plugins: [
-        replace({
-          'process.env.NODE_ENV': JSON.stringify('production'),
-        }),
-        terser({
-          compress: { passes: 2, module: true, unsafe: true },
-        }),
-      ],
-    },
-  ],
+  output,
+  plugins,
 }
