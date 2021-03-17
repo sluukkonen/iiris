51bfcb00f4b16610d8b0bd1637ce8a1852066ee4
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2021-03-17 21:10:52 +0200

    Remove sortWith optimizations
    They don't seem like they're worth the extra complexity and code size.
    If a user wants to optimize their composite comparator function, they
    can do it by hand and use I.sort.

--- src/internal/sortWithU.js
+++ src/internal/sortWithU.js
@@ -1,5 +1,13 @@
-import { combineComparators } from './combineComparators'
 import { sortU } from './sortU'
 
 export const sortWithU = (comparators, array) =>
-  sortU(combineComparators(comparators), array)
+  sortU((a, b) => {
+    let result = 0
+
+    for (const comparator of comparators) {
+      result = comparator(a, b)
+      if (result !== 0) break
+    }
+
+    return result
+  }, array)
