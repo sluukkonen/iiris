d1fa3b8539aac12ce590759fa32c91f704a891db
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2021-03-21 09:39:54 +0200

    Remove dead code

--- src/internal/copyObject.js
+++
@@ -1,8 +0,0 @@
-import { isObject } from '../isObject'
-
-export const copyObject = (object) => {
-  if (!isObject(object)) {
-    throw new TypeError('Expected an object')
-  }
-  return { ...object }
-}
