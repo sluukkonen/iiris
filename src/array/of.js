c37c0fc6be23c906843f873d314ee3f1d6644555
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2021-04-11 20:56:52 +0300

    Rename A.of to A.singleton

--- src/array/of.js
+++
@@ -1,1 +0,0 @@
-export const of = (a) => [a]
--- src/array/of.test.js
+++
@@ -1,6 +0,0 @@
-import { of } from './of.js'
-
-it('creates a singleton array from the first argument', () => {
-  expect(of(1)).toEqual([1])
-  expect(of(2)).toEqual([2])
-})
